8256c9441e8da6b54efa86fe7fab3b8b2c65b538
Alan Medina <realconect@gmail.com>
2013-07-13 00:11:27 -0500

    Se ha creado una app en Rails y se ha agregado el modelo status

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Feisbuk::Application.config.secret_token = 'ffeca55a15265a02c4fd6ef69258715ea9dfd49e4b21e7da9e715093450213ee4dab0c57d25cb3239736a0965982c41a20594ab9727fffd4897e0d306b242280'
